61bd8c8e5edb1c535f3318465139d97b008176f4
Fikri Ariqsya <fikriariqsyarahman98@gmail.com>
2022-04-17 21:00:04 +0700

    add hapusData[working] and updateData[not working]

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,25 @@ Route::get('/berandaMitra', function () {
     return view('berandaMitra');
 });
 
+Route::get('/berandaPegawai', function () {
+    return view('berandaPegawai');
+});
+
 Route::get('/profilMitra', function () {
     return view('profilMitra');
 });
 
 Route::get('/ubahProfil', function () {
-    return view('ubahProfil');
-});
+    return view('ubahProfil');});
+
+// Route untuk update data profil
+Route::put('/profilMitra/update', [ProfileController::class, 'update']);
 
 // Route menampilkan profil pegawai
 Route::get('/profilPegawai', [ProfileController::class, 'index']);
+
+// Route untuk delete
+Route::post('/profilPegawai/hapus', [ProfileController::class, 'hapus']);
 
 // REGISTER
 // Menampilkan halaman register
